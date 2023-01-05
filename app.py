8093cfbf4c2ad599326d0d6f0a8ed777eb28e690
Casey Collins <kcc989@gmail.com>
2023-01-04 19:23:18 -0500

    improve performance

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
 import beam
 
+# The environment your code will run on
 app = beam.App(
-    name="stable-diffusion-video-example",
-    cpu=4,
-    gpu=1,
+    name="stable-diffusion-app",
+    cpu=8,
     memory="32Gi",
+    gpu=1,
     python_version="python3.8",
-    python_packages=["diffusers", "transformers", "torch", "pillow", "accelerate"],
+    python_packages=[
+        "diffusers[torch]>=0.10",
+        "transformers",
+        "torch",
+        "pillow",
+        "triton",
+        "accelerate",
+        "xformers==0.0.16rc393",
+        "safetensors",
+    ],
 )
 
+# Deploys function as async webhook
 app.Trigger.Webhook(
-    inputs={"prompt": beam.Types.String()}, handler="run.py:generate_image"
+    inputs={"prompt": beam.Types.String()},
+    handler="run.py:generate_image",
 )
 
-app.Output.File(path="output.png", name="my_image")
+# File to store image outputs
+app.Output.File(path="output.png", name="myimage")
 
+# Persistent volume to store cached model
 app.Mount.PersistentVolume(app_path="./cached_models", name="cached_model")
